07f92ee97965b0915fc70ff81c0834430ee14619
bond_kaneko <jyunli5088@gmail.com>
2025-12-31 13:23:18 +0900

    Implement OIDC configuration endpoint with dynamic issuer and update environment settings; remove deprecated nginx configuration file

--- id-provider/app/controllers/oidc/well_known_controller.rb
+++ id-provider/app/controllers/oidc/well_known_controller.rb
@@ -1,7 +1,19 @@
 # app/controllers/oidc/well_known_controller.rb
 class Oidc::WellKnownController < Oidc::ApplicationController
   def configuration
-    # OpenID Connect Discovery 情報の返却
-    render json: {}
+    issuer = request.base_url.gsub(/\/$/, '')
+    
+    render json: {
+      issuer: issuer,
+      authorization_endpoint: "#{issuer}/oidc/authorize",
+      token_endpoint: "#{issuer}/oidc/token",
+      userinfo_endpoint: "#{issuer}/oidc/userinfo",
+      jwks_uri: "#{issuer}/oidc/jwks",
+      response_types_supported: ["code"],
+      subject_types_supported: ["public"],
+      id_token_signing_alg_values_supported: ["RS256"],
+      scopes_supported: ["openid", "profile", "email"]
+    }
+  end
   end
 end
